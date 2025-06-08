daa28913e2c089526d22e858eefba678ec1d82bc
Thangle2108 <thanglesy00005@gmail.com>
2025-06-08 16:59:12 +0700

    Define fillable fields and relationships

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'image',
+        'link',
+        'position',
+        'starts_at',
+        'ends_at',
+    ];
 }
