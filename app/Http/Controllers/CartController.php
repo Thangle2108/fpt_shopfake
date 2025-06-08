d9ec40331773f27faf0fc66b9bbc7f5fcd196752
Thangle2108 <thanglesy00005@gmail.com>
2025-06-08 20:06:27 +0700

    Add basic cart controller methods

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    /**
+     * Display the shopping cart page.
+     */
+    public function index()
+    {
+        return view('cart.index');
+    }
+
+    /**
+     * Add an item to the cart.
+     */
+    public function add(Request $request)
+    {
+        return response()->json(['message' => 'Item added to cart']);
+    }
+
+    /**
+     * Complete the checkout process.
+     */
+    public function checkout(Request $request)
+    {
+        return response()->json(['message' => 'Checkout completed']);
+    }
 }
